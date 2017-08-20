0c4447a4e2dc47cf826f8db0469df106f41aac01
Tomas <tomasmarques2@gmail.com>
2017-08-20 21:08:54 +0100

    Fixed some missing files

---
+++ src/engine/ui/Renderable.java
@@ -0,0 +1,10 @@
+package engine.ui;
+
+import java.awt.*;
+
+/**
+ * Created by tomas on 14/07/2017.
+ */
+public interface Renderable {
+    public void render(Graphics g);
+}
---
+++ src/engine/window/Window.java
@@ -0,0 +1,129 @@
+package engine.window;
+
+import engine.ui.*;
+import javafx.stage.Screen;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+/**
+ * Created by tomas on 4/23/2017.
+ */
+public abstract class Window extends JFrame {
+    private boolean antialiasing = true;
+    private Display display = new Display();
+    private ArrayList<Renderable> renderQueue = new ArrayList<>();
+
+    public Window(String title, int width, int height) {
+        super(title);
+
+        display.addMouseListener(new MouseHandler());
+        add(display);
+        setSize(width, height);
+        setIgnoreRepaint(false);
+        setResizable(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public final void clearRenderableQueue(){
+        repaint();
+    }
+    public abstract void update();
+
+    public final void mouseUpdate(){
+        checkMouseDragging();
+        checkMouseOver();
+    }
+
+    private void checkMouseOver(){
+        if(display.getMousePosition() != null){
+            Rectangle mouse = new Rectangle(display.getMousePosition().x, display.getMousePosition().y, 1, 1);
+            for(UIElement uie : UIElement.getInstances()){
+                if(uie instanceof Highlightable){
+                    if(mouse.intersects(uie.getSprite().getCollisionBox().getBounds()))
+                        ((Highlightable) uie).setMouseOver(true);
+                    else
+                        ((Highlightable) uie).setMouseOver(false);
+                }
+            }
+        }
+    }
+    private void checkMouseDragging(){
+        for(UIElement uie : UIElement.getInstances())
+            if(uie instanceof Draggable)
+                if(((Draggable) uie).getDragMode() != UIElement.DRAG_NONE)
+                    if(display.getMousePosition() != null)
+                        ((Draggable) uie).drag(display.getMousePosition().x, display.getMousePosition().y);
+    }
+    private Graphics checkAntialiasing(Graphics g){
+        if(this.antialiasing){
+            ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        }
+        return g;
+    }
+    public final void display(){
+        setVisible(true);
+    }
+    public final void setAntialiasing(boolean antialiasing){
+        this.antialiasing = antialiasing;
+    }
+    public final void center(){
+        setLocationRelativeTo(null);
+    }
+    public final void queueRenderable(Renderable renderable){
+        renderQueue.add(renderable);
+    }
+
+    private class Display extends JPanel{
+        @Override
+        public void paintComponent(Graphics g){
+            cls(g);
+            g = checkAntialiasing(g);
+            for(Renderable r : renderQueue)
+                r.render(g);
+            renderQueue = new ArrayList<>();
+        }
+
+        private void cls(Graphics g){
+            g.clearRect(0, 0, getWidth(), getHeight());
+        }
+    }
+    private class MouseHandler implements MouseListener {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            for(UIElement uie : UIElement.getInstances()){
+                if(uie instanceof Clickable){
+                    ((Clickable) uie).clicked(e.getButton(), e.getX(), e.getY());
+                }
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            for(UIElement uie : UIElement.getInstances()){
+                if(uie instanceof Draggable){
+                    if(((Draggable) uie).getDragMode() == UIElement.DRAG_CLICK_HOLD_RELEASE)
+                        ((Draggable) uie).setDragging(false);
+                }
+            }
+        }
+    }
+}
